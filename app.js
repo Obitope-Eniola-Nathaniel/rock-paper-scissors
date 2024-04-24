39d7ff5328a67062e5bebebbd1554e9ccd889cbd
Obitope Eniola Nathaniel <obitopeeniola@gamil.com>
2024-04-24 09:19:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+
+
+
+function computerChoice() {
+    const images = ['rock.png', 'paper.png', 'scissors.png']
+    const random = Math.floor(Math.random() * images.length)
+
+    const imgElement = document.querySelector("#computer-pick")
+    const computerText = document.querySelector(".computer-plays")
+    // imgElement.setAttribute('src', images[random])
+    computerText.innerHTML = 'computer plays'
+    imgElement.src = images[random]
+    return imgElement
+  
+}
+
+
+let image = document.querySelectorAll(".pic")
+image.forEach((item) => {
+    item.addEventListener("click", () => {
+         
+        computer = computerChoice()
+        computerPlay = computer.getAttribute('src')
+        player = item.getAttribute('src')
+
+        if (player === computerPlay) {
+            console.log("Its a tie!")
+        } 
+        else if (player === "rock.png" || computerPlay === "scissors.png") {
+            console.log("You win")
+        }
+        else if (player === "scissors.png" || computerPlay === "paper.png") {
+            console.log("You win")
+        }
+        else if (player === "paper.png" || computerPlay === "rock.png") {
+            console.log("You win")
+        }
+        else {
+            console.log("You lose!")
+        }
+
+        console.log(item.getAttribute('src'))
+        console.log(computer.getAttribute('src'))
+      
+    })
+
+})
